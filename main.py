cf2b7fe935e1cf9d26f3d195f86eb4740f8e4633
Egor Kuzmichev <GarretTheShadow@users.noreply.github.com>
2019-05-13 16:35:35 +0300

    First committing of my file manager

--- main.py
+++ main.py
@@ -4,23 +4,50 @@ import os
 
 
 def change_directory(new_directory):
+
     os.changedir(new_directory)
+    return
 
 
 def move_up_folder():
+
     current_path = os.getcwd()
-    updated_path = current_path.rfind('\\')
-    return current_path[0:updated_path]
+    if os.name == 'nt' and len(current_path) > 3:
+        updated_path = current_path.rfind('\\')
+        return current_path[0:updated_path]
+
+    else:
+        print('Other OS unsupported yet')
+    return
+
+def make_directory(path):
+
+    os.makedirs(path)
+    return
 
 
-def rename_file(input_name, output_name):
+def delete_directory(path):
 
-    if os.name == 'nt':
-        os.rename(input_name, output_name)
-        print('File renamed into: '+output_name)
+    os.removedirs(path)
+    print('Folder '+path+'successfully deleted')
+    return
 
-    elif os.name == 'posix':
+def delete_file(path):
+
+    os.remove(path)
+    return
+
+
+def rename_file(src, dst):
+
+        os.rename(src, dst)
+        print(src+' Renamed into: '+dst)
         return
+
+
+def copy_file(src, dst):
+
+    #comming soon
 
 
 move_up_folder()
